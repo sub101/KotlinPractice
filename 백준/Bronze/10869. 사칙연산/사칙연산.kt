14018e6c3d4aeebabf84dd3e9c43d192dce32dfb
SUBIN KIM <58244158+sub101@users.noreply.github.com>
2022-12-02 14:41:49 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 13540 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,15 @@
+import java.util.*
+//11720
+fun main(args:Array<String>){
+    val sc : Scanner = Scanner(System.`in`)
+
+    var A : Int = sc.nextInt()
+    var B : Int = sc.nextInt()
+
+    println(A+B)
+    println(A-B)
+    println(A*B)
+    println(A/B)
+    println(A%B)
+
+}
