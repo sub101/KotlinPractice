cff3a755845aa73a23b7e4284f66e8da76e8686e
SUBIN KIM <58244158+sub101@users.noreply.github.com>
2023-03-02 15:54:53 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,62 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-	// 1)
-	static int maxNum(int a, int b, int c) {
-		int max = 0;
-		if(a > b) {
-			if(b > c)
-				max = a;
-			else 
-				max = (a>c) ? a : c;
-		}
-		
-		else 
-			max = (b>c) ? b : c;
-		return max;
-	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		while(true) {
-			int a = scan.nextInt();
-			int b = scan.nextInt();
-			int c = scan.nextInt();
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+			int num3 = scan.nextInt();
 			String result = "";
-			if(a == 0 && b == 0 && c == 0)
+			if(num1 == 0 && num2 == 0 && num3 == 0)
 				break;
 			
-			int max = maxNum(a, b, c);	// a, b, c 중 최댓값 찾기
-			
-			// 2)
-			if(a == max) {
-				if(a >= b+c)
-					result = "Invalid";
-			}
-			else if(b == max) {
-				if(b >= a+c)
-					result = "Invalid";
-			}
-			else {
-				if(c >= a+b)
-					result = "Invalid";
+			int [] arr = {num1,num2,num3};
+			Arrays.sort(arr);
+
+			if(arr[2]>= arr[0]+arr[1]) {
+				result = "Invalid";
 			}
 			
-			// 3)
-			if(result == "" && a == b && b == c )
+			if(result == "" && arr[0]==arr[1] && arr[1] == arr[2] )
 				result = "Equilateral";
-			else if(result == "" && (a == b && b != c) || (a == c && b != c) || (b == c && a != b))
+			else if(result == "" && (arr[0] == arr[1] && arr[1] != arr[2]) || (arr[0] == arr[2] && arr[1] != arr[2]) || (arr[1] == arr[2] && arr[0] != arr[1]))
 				result = "Isosceles";
-			else if(result == "" && a != b && b != c && a != c)
+			else if(result == "" && arr[0] != arr[1] && arr[1] != arr[2] && arr[0] != arr[2])
 				result = "Scalene";
 			
 			System.out.println(result);
 				
 			
 		}
-		scan.close();
 	}
 
 }
