f65eaa02f7501e1be2c8ab0d43c6a48ab8e75fa0
SUBIN KIM <58244158+sub101@users.noreply.github.com>
2023-08-19 23:40:15 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(number: Int, n: Int, m: Int): Int {
+        var answer: Int = 0
+        
+        if(number%n==0 && number%m==0) {
+            answer = 1
+        }else{
+            answer = 0
+        }
+        return answer
+    }
+}
