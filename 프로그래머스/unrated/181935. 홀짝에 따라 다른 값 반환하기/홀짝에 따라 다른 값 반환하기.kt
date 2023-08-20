4c8632f99b4d339527dd7bcfd62c07cf70bd776f
SUBIN KIM <58244158+sub101@users.noreply.github.com>
2023-08-20 22:29:05 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.22 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        
+        if(n%2==0){
+            for(i in 2 .. n step 2){
+                answer = i*i+answer
+            }
+        }else{
+            for(i in 1 .. n step 2) {
+                answer += i
+            }
+        }
+        return answer
+    }
+}
