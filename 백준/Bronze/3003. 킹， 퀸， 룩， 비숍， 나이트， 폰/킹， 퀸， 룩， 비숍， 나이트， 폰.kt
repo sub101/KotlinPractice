b85aed9b632dacdfba38d6564f22fd5a37d85ca8
SUBIN KIM <58244158+sub101@users.noreply.github.com>
2023-10-17 23:58:15 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 132 ms, Memory: 21244 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    println( readLine()
+        .split(" ")
+        .mapIndexed { index, ea ->
+            movementCheck(index, ea.toInt()) }
+        .joinToString(" ")
+    )
+}
+
+fun movementCheck(index: Int, movementEA: Int): Int {
+    return when (index) {
+        0 -> 1 - movementEA
+        1 -> 1 - movementEA
+        2 -> 2 - movementEA
+        3 -> 2 - movementEA
+        4 -> 2 - movementEA
+        else -> 8 - movementEA
+    }
+}
